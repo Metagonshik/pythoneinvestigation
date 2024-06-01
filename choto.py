48a1b3fd0a90fc71726b2235318992ef599d0000
Ruslan Batchaev <r89654404324@gmail.com>
2024-06-01 10:22:25 +0300

    Простой роутер с фласком

--- choto.py
+++ choto.py
@@ -1,3 +1,15 @@
-def da(x, y):
-    return(x + y)
-print(da(1, 2))
+import requests
+
+st_accept = "text/html" 
+st_useragent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 12_3_1) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.4 Safari/605.1.15"
+
+headers = {
+   "Accept": st_accept,
+   "User-Agent": st_useragent
+}
+
+req = requests.get("https://yandex.ru", headers)
+
+src = req.text
+print(src)
+
