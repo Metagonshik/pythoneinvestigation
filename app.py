1510fab2308d4e8b58ed7ec8109b9c8d0be10917
Ruslan Batchaev <r89654404324@gmail.com>
2024-06-01 10:57:54 +0300

    Working form

--- app.py
+++ app.py
@@ -1,11 +1,24 @@
-from flask import Flask
+from flask import Flask, request
 from parserYa import parseyandex
 
 app=Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    page='''
+    <html>
+        <form action="/formProcess" method="POST">
+            <input type=text name="myText">
+            <input type="submit" value="Сохранить">
+        </form>
+    </html>
+    '''
+    return page
+
+@app.route("/formProcess", methods=['GET','POST'])
+def formProcessing():
+    return request.form['myText']
+
 
 @app.route("/start")
 def asdfas():
